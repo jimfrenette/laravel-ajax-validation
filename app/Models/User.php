274af6c721792d38ea9968f3dc7b8ec67c9fd5fd
U-VGN-FW290\admin <jimfrenette@yahoo.com>
2016-05-21 23:17:09 -0400

    first page

---
+++ app/Models/User.php
@@ -0,0 +1,40 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Foundation\Auth\User as Authenticatable;
+
+class User extends Authenticatable
+{
+    /**
+     * Get the validation rules that apply to the user.
+     *
+     * @return array
+     */
+    public function rules()
+    {
+        return [
+            'name' => 'required|max:255',
+            'email' => 'required|email|max:255|unique:users',
+            'password' => 'required|min:6|confirmed',
+        ];
+    }
+
+    /**
+     * The attributes that are mass assignable.
+     *
+     * @var array
+     */
+    protected $fillable = [
+        'name', 'email', 'password',
+    ];
+
+    /**
+     * The attributes that should be hidden for arrays.
+     *
+     * @var array
+     */
+    protected $hidden = [
+        'password', 'remember_token',
+    ];
+}
---
+++ readme.md
@@ -0,0 +1,3 @@
+# Laravel User Authentication with Ajax Validation
+
+This repository contains the Laravel project files that were modified when adding Ajax form validation to Laravel User Authentication generated with artisan.
